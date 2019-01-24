0490b128ea769350e26c07ea321ef9c13d69cbba
reco_luan <1156743527@qq.com>
2019-01-24 14:35:09 +0800

    Update index.vue

--- components/Category/index.vue
+++ components/Category/index.vue
@@ -70,9 +70,9 @@ export default {
 @require '../../styles/wrapper.styl'
 
 .categories-wrapper
-  padding: 0rem 1.5rem 0 !important;
-  max-width: 960px;
-  margin: 0px auto;
+  max-width: 740px;
+  margin: 0 auto;
+  padding: 0 2.5rem; 
 
 @media (max-width: $MQMobile)
   .categories-wrapper
