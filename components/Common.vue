43917801f589e8e4344bf221348eeb275811a5be
reco_luan <luanhewei@gmail.com>
2019-05-05 11:47:30 +0800

    修复bug

--- components/Common.vue
+++ components/Common.vue
@@ -141,7 +141,7 @@ export default {
         return
       }
 
-      this.isHasPageKey = pageKeys && pageKeys.indexOf(Number(sessionStorage.getItem('pageKey'))) > -1
+      this.isHasPageKey = pageKeys && pageKeys.indexOf(sessionStorage.getItem('pageKey')) > -1
       console.log(123, this.isHasPageKey, pageKeys)
     },
     toggleSidebar (to) {
