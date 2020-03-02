0b90ca257211e50ba699c4423527c7a88899b0c0
franklinqin0 <franklinqin0@gmail.com>
2020-03-03 01:49:03 +0800

    add custom blocks: right, theorem, details

--- index.js
+++ index.js
@@ -58,19 +58,40 @@ module.exports = (options, ctx) => ({
     ['container', {
       type: 'tip',
       defaultTitle: {
+        '/': 'TIP',
         '/zh/': '提示'
       }
     }],
     ['container', {
       type: 'warning',
       defaultTitle: {
+        '/': 'WARNING',
         '/zh/': '注意'
       }
     }],
     ['container', {
       type: 'danger',
       defaultTitle: {
+        '/': 'WARNING',
         '/zh/': '警告'
+      }
+    }],
+    ['container', {
+      type: 'right',
+      defaultTitle: ''
+    }],
+    ['container', {
+      type: 'theorem',
+      before: info => `<div class="custom-block theorem"><p class="title">${info}</p>`,
+      after: '</div>',
+    }],
+    ['container', {
+      type: 'details',
+      before: info => `<details class="custom-block details">${info ? `<summary>${info}</summary>` : ''}\n`,
+      after: () => '</details>\n',
+      defaultTitle: {
+        '/': 'See More',
+        '/zh/': '更多'
       }
     }]
   ]
