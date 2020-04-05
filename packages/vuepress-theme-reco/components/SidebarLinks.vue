baeec51a43322000042a37845d2b7a0a34948eee
z <916665067@qq.com>
2020-04-05 12:43:55 +0800

    fix the sidebar title of beyond the current window that it follows the title of the article in the current window

--- packages/vuepress-theme-reco/components/SidebarLinks.vue
+++ packages/vuepress-theme-reco/components/SidebarLinks.vue
@@ -52,8 +52,33 @@ export default {
       this.refreshIndex()
     }
   },
+  
+  mounted() {
+    this.isInViewPortOfOne()
+  },
+  
+  updated: function () {
+    this.isInViewPortOfOne()
+  },
 
   methods: {
+    isInViewPortOfOne () {
+        let siderbarScroll = document.getElementsByClassName("sidebar")[0]
+        let el = document.getElementsByClassName("active sidebar-link")[1]
+        if (el ==null || el.offsetTop == undefined) {
+          el = document.getElementsByClassName("active sidebar-link")[0]
+        }
+        if (el ==null || el.offsetTop == undefined) return
+        
+        const viewPortHeight = siderbarScroll.clientHeight || window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight 
+        let offsetBottom = el.offsetTop + el.offsetHeight
+        let scrollTop = siderbarScroll.scrollTop
+        let isView = (offsetBottom <= viewPortHeight + scrollTop)
+        if (!isView) {
+          siderbarScroll.scrollTop = (offsetBottom+5 - viewPortHeight)
+        }
+    },
+    
     refreshIndex () {
       const index = resolveOpenGroupIndex(
         this.$route,
