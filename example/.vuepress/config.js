0a7acd8bf7597932bbf41494f8f08efd56321854
luanhewei <luanhewei@meituan.com>
2020-07-09 13:27:42 +0800

    test(vuepress-theme-reco): add test script

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
+  base: '/reco-example-test/',
   // theme: 'reco',
   theme: require.resolve('../../packages/vuepress-theme-reco'),
   themeConfig: {
