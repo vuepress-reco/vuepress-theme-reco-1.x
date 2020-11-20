1d0cedf427da34b1bc3920beea91f2f71de5ba10
chenjib <chenjibzh@163.com>
2020-11-20 17:25:53 +0800

    feat(vuepress-theme-reco): change import format

--- packages/@vuepress-reco/core/index.js
+++ packages/@vuepress-reco/core/index.js
@@ -1,1 +1,20 @@
-export { RecoIcon } from './components/'
+import * as components from "./components";
+import * as tools from "./tools"
+import * as mixins from "./mixins"
+import * as directives from "./directives"
+
+const core = {
+  tools,
+  components,
+  mixins,
+  directives
+}
+
+export default core
+
+export {
+  tools,
+  components,
+  mixins,
+  directives
+}
