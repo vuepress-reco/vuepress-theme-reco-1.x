a070ea7bd8343b49db788d40aaf500773bd6ec91
reco_luan <recoluan@outlook.com>
2021-12-04 11:34:51 +0800

    refactor: use font files instead of cdn

--- packages/vuepress-theme-reco/enhanceApp.js
+++ packages/vuepress-theme-reco/enhanceApp.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-proto */
 import postMixin from '@theme/mixins/posts'
 import localMixin from '@theme/mixins/locales'
-import { addLinkToHead, addScriptToHead } from '@theme/helpers/utils'
+import { addScriptToHead } from '@theme/helpers/utils'
 import { registerCodeThemeCss, interceptRouterError, fixRouterError404 } from '@theme/helpers/other'
 import { install } from 'vue-demi'
 
@@ -15,7 +15,6 @@ export default ({
   Vue.mixin(postMixin)
   Vue.mixin(localMixin)
   if (!isServer) {
-    addLinkToHead('//at.alicdn.com/t/font_1030519_2ciwdtb4x65.css')
     addScriptToHead('//kit.fontawesome.com/51b01de608.js')
     registerCodeThemeCss(siteData.themeConfig.codeTheme)
   }
