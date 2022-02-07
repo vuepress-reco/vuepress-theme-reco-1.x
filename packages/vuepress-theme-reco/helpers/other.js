6e2775d5f1a547a483769e21c90a3fde5935b5b9
sugar <engineerzjl@foxmail.com>
2022-02-07 22:58:50 +0800

    feat: 解决permalink中使用slug 的中文路由跳转失败问题

--- packages/vuepress-theme-reco/helpers/other.js
+++ packages/vuepress-theme-reco/helpers/other.js
@@ -38,10 +38,23 @@ export function interceptRouterError (router) {
 }
 
 export function fixRouterError404 (router) {
+  const routes = router.getRoutes()
   router.beforeEach((to, from, next) => {
-    // 解决中文标签/分类路由无法被正确加载的问题
+    // 解决decode后反复重定向的问题
+    const route = routes.find((v) => v.regex.test(to.path))
+
     // .html有重定向路由，无需特殊处理
-    if (!/\.html$/.test(to.path) && decodeURIComponent(to.path) !== to.path) {
+    if (/\.html$/.test(to.path)) {
+      return next()
+    }
+
+    // 不特殊处理 非通配符的 没有重定向的路由
+    if (route && route.path !== '*' && !route.redirect) {
+      return next()
+    }
+
+    // 解决中文标签/分类路由无法被正确加载的问题
+    if (decodeURIComponent(to.path) !== to.path) {
       return next(
         Object.assign({}, to, {
           path: decodeURIComponent(to.path),
@@ -49,6 +62,7 @@ export function fixRouterError404 (router) {
         })
       )
     }
+
     next()
   })
 }
